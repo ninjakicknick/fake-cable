732e2e0f20759fd08c5def5e29f2683c740946e7
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-04 17:15:01 -0400

    Refresh guide control cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fake-cable-shell-v14';
+const CACHE='fake-cable-shell-v15';
 const SHELL=['/','/styles.css','/app.js','/schedule.js','/lineup-share.js','/themes.js','/vendor/qrcode.min.js','/vendor/peerjs.min.js','/manifest.webmanifest','/icons/fake-cable-192.svg','/icons/fake-cable-512.svg'];
 
 self.addEventListener('install',event=>{
