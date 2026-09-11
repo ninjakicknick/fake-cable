b87d8ec3b9fa59996876cb80dc15884ed7d57c99
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-11 18:23:48 -0400

    Add Wu Tang Collection and KEXP starter channels

--- storage.js
+++ storage.js
@@ -10,7 +10,7 @@ export const STORAGE_KEYS=Object.freeze({
   lastChannel:'elsewhere-last-channel'
 });
 
-export const LINEUP_VERSION='10';
+export const LINEUP_VERSION='11';
 export const CATALOG_VERSION='3';
 
 function clone(value){
