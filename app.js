777bf94a388fda58a476760d9e6b902c3606525e
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-12 11:54:21 -0400

    Extract starter channel catalog from app controller

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import {DEFAULT_STATIONS,DEFAULT_COMMERCIAL_PLAYLIST,DEMO_CHANNELS} from './catalog.js';
 import {currentIndexAt,makeSchedule} from './schedule.js';
 import {createPlayerController} from './player.js';
 import {createTuningStatic} from './tuning-static.js';
@@ -23,78 +24,6 @@ window.addEventListener('appinstalled',()=>{
   document.querySelector('#install-app')?.classList.add('hidden');
 });
 if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}));
-const DEFAULT_STATIONS=[
- {value:'https://www.youtube.com/playlist?list=PLRmpOEZ5F1SA',label:'Weird Animation',tags:['animation','weird']},
- {value:'https://www.youtube.com/playlist?list=PLTkogaZu8Kn0',label:'Horror Shorts',tags:['horror']},
- {value:'https://www.youtube.com/playlist?list=PLbBSn4qd5pFE',label:'Documentaries',tags:['documentary']},
- {value:'https://youtube.com/@bobross_thejoyofpainting',label:'Bob Ross',tags:[]},
- {value:'https://youtube.com/@supersaturdaymorning',label:'Super Saturday Morning',tags:['animation']},
- {value:'https://youtube.com/@wutangcollectiondope',label:'Wu Tang Collection',tags:[]},
- {value:'https://youtube.com/@kexp',label:'KEXP',tags:['music']},
- {value:'https://youtube.com/@dailydoseofinternet',label:'Daily Dose of Internet',tags:[]}
-];
-const DEFAULT_COMMERCIAL_PLAYLIST={
- value:'https://www.youtube.com/playlist?list=PLVKuQ0TpzAbfIbgfIAQOQs9Y7e6dRjknw',
- label:'Classic TV Commercials'
-};
-const DEMO_CHANNELS=[
- {n:2,name:'WEIRD ANIMATION',channelId:'playlist:PLRmpOEZ5F1SA',playlistId:'PLRmpOEZ5F1SA',color:'#b18cff',shows:[
-  ['DISTORTION. A Stop motion Animation by Guldies','Guldies','tZqIQmdSa1E',105],
-  ['DOUBLE KING','Felix Colgrave','w_MSFkZHNi4',587],
-  ['late for meeting','David Lewandowski','wBqM2ytqHY4',102],
-  ['CGI Animated Short Film: "Don\'t Croak" by Daun Kim | CGMeetup','CGMeetup','j8PDTJNaPc0',101],
-  ['Fantasy Haircut','DoodletmeGO','P6uwj5Ix7qg',315],
-  ['Fuelled | Animated Short Film 2021','Killedthecat Productions','uVzT6D-yhZg',543],
-  ['where_did_life_go','Doctor Nowhere','0DVdDa0qKoc',150],
-  ['Best Friend - Animation Short Film 2018 - GOBELINS','GOBELINS Paris','j01Hg4QJ6NE',347],
-  ['cat city','vewn','NWeyUpqnKuk',181],
-  ['Coda','and maps and plans','MkA3sLyEWdU',541],
-  ['A Brief Disagreement','Steve Cutts','9x7FGbW3IVc',186],
-  ['Pinched','Titmouse','oWlhMekUZRs',675]
- ]},
- {n:3,name:'HORROR SHORTS',channelId:'playlist:PLTkogaZu8Kn0',playlistId:'PLTkogaZu8Kn0',color:'#ed6a5a',shows:[
-  ['Other Side of the Box | Award-Winning Horror Short Film','Short of the Week','TGZg1YqXv9o',950],
-  ['Portrait of God (Short Horror Film)','Dylan Clark','BI9fKfX5V68',450],
-  ['POSSIBLY IN MICHIGAN (1983)','ceceliacondit','iLJNSD3H5sg',706],
-  ['THE CHAIR (Award Winning Horror Short Film Directed by Curry Barker)',"that's a bad idea",'mhazCS14Tas',1463],
-  ['This House Has People in It','Adult Swim','x-pj8OtyO2I',715],
-  ['Unedited Footage of a Bear | Infomercials | Adult Swim','Adult Swim','2gMjJNGg9Z8',629],
-  ['Too Many Cooks | Adult Swim','Adult Swim','QrGrOK8oZG8',672],
-  ['GUEST — A Horror Short Film by Finn Callan','Finn Callan','7-5Upq2hcOA',678],
-  ['THE SKY - AWARD WINNING COSMIC HORROR','Matt Sears','ln4lDjT8Ab0',674],
-  ['Mama Agnes - Short Horror Film','Alexanderthetitan','IfgesY42oN8',165],
-  ['Horror Short Film “Backstroke” | ALTER','ALTER','3_DQmfFjTlU',622],
-  ['SCP: Containment Breach - The Movie | SCP-173 Live Action','Keter Labs','Fw4lLEfgQuo',715],
-  ['The Flying Man','Marcus Alqueres','Gj1MqHgFnmE',561],
-  ['Not My Dog | Horror Short Film','CIAK COMPANY','_QlUUb5UMvA',327]
- ]},
- {n:4,name:'DOCUMENTARIES',channelId:'playlist:PLbBSn4qd5pFE',playlistId:'PLbBSn4qd5pFE',color:'#63a7ff',shows:[
-  ["SATAN'S GUIDE TO THE BIBLE",'SATANSGUIDE','z8j3HvmgpYc',5151],
-  ["MyHouse.WAD - Inside Doom's Most Terrifying Mod",'Power Pak','5wAo54DHDY0',6121],
-  ["TaeKwonDo Rockers vs. Cocaine-Dealing Ninjas: The True Story of 'Miami Connection'",'VICE','z1UmZ4WWspo',1395],
-  ["The Incredibly Strange, Sad Story Behind The World's Most Ambitious Demo Tape",'VICE','1pI24MkekW8',1574],
-  ["On The Road with The World's Most Hyperactive Horror Director",'VICE','c62Ot4L7m94',1496],
-  ["The Worst Movie Ever Made? The True Story of 'Birdemic'",'VICE','uZoFNVhEfpE',1589],
-  ['The Mayhem of Jason Miller | Dark Side Of The Cage','VICE and 2 more','3Zc5SrQuZXY',2669],
-  ["SoCal Skate History And Eating At Tony Hawk's Place with Lizzie Armanto",'VICE','SRkP2ewEWMU',1037],
-  ['The Series That Changed Skateboarding Forever | Let It Kill You','VICE','ZL5l9iEI0uE',2734],
-  ['Rule Britannia: Inside Britain’s Shoplifting Epidemic','VICE','fJOeJ-BqjCg',1266]
- ]},
- {n:5,name:'BOB ROSS',channelId:'UCxcnsr1R5Ge_fbTu5ajt8DQ',color:'#75d887',shows:[
-  ['Gladiolus with CRI® Doug Hallgren | Brush Strokes','Bob Ross','BIEppZQRNWk',1884],
-  ['Bob Ross - One Hour Special - Peace Offerings of Summer','Bob Ross','NDpKTkdRBu0',3545],
-  ["Ellen's Beach with CRI® Nicholas Hankins | Brush Strokes",'Bob Ross','LRpaBxkYqaU',2909],
-  ["Brush Strokes Table Talk | Episode 11 | Bob Ross' Most Iconic Quotes",'Bob Ross','kyW35uDde84',1325],
-  ['Twilight Meadow with CRI® Doug Hallgren | Brush Strokes','Bob Ross','PqJQc4BVgUE',1973],
-  ['Happy Little Threes: Summer Bob | Episode 82 | The Joy of Bob Ross: A Happy Little Podcast®','Bob Ross','twH7MR2QJK8',1579],
-  ['Daffodil with CRI® Carolyn Saletto | Brush Strokes','Bob Ross','ShEx_7hlCfk',2430],
-  ['Bob Ross: Top 10 Summer Paintings','Bob Ross','Uk9BksOB8a4',15430],
-  ['A Day at the Beach with CRI® Nicholas Hankins | Brush Strokes','Bob Ross','HbGMWmAR2gk',1750],
-  ["A Few Of Bob's Favorite Things | Episode 81 | The Joy of Bob Ross: A Happy Little Podcast®",'Bob Ross','OmgZ49F9O8k',1292],
-  ['Brush Strokes Table Talk | Episode 10 | Painting Inspiration','Bob Ross','KGYpu61BGAA',1509],
-  ['Peachy Rose with CRI® Doug Hallgren | Brush Strokes','Bob Ross','EL5C1TWKWYU',2358]
- ]}
-];
 let SOURCE_CHANNELS=loadSourceChannels(localStorage,DEMO_CHANNELS);
 let CHANNELS=[];
 let mixes=loadMixes(localStorage);
