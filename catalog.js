43cab18a3f3191b7e624c3a1a705335ad903d33e
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-14 19:47:12 -0400

    Replace default commercial playlist

--- catalog.js
+++ catalog.js
@@ -9,7 +9,7 @@ export const DEFAULT_STATIONS=[
  {value:'https://youtube.com/@dailydoseofinternet',label:'Daily Dose of Internet',tags:[]}
 ];
 export const DEFAULT_COMMERCIAL_PLAYLIST={
- value:'https://www.youtube.com/playlist?list=PLVKuQ0TpzAbfIbgfIAQOQs9Y7e6dRjknw',
+ value:'https://www.youtube.com/playlist?list=PLD6MJW3gqM9A',
  label:'Classic TV Commercials'
 };
 export const DEMO_CHANNELS=[
@@ -17,7 +17,7 @@ export const DEMO_CHANNELS=[
   ['DISTORTION. A Stop motion Animation by Guldies','Guldies','tZqIQmdSa1E',105],
   ['DOUBLE KING','Felix Colgrave','w_MSFkZHNi4',587],
   ['late for meeting','David Lewandowski','wBqM2ytqHY4',102],
-  ['CGI Animated Short Film: "Don\'t Croak" by Daun Kim | CGMeetup','CGMeetup','j8PDTJNaPc0',101],
+  ['CGI Animated Short Film: \"Don\'t Croak\" by Daun Kim | CGMeetup','CGMeetup','j8PDTJNaPc0',101],
   ['Fantasy Haircut','DoodletmeGO','P6uwj5Ix7qg',315],
   ['Fuelled | Animated Short Film 2021','Killedthecat Productions','uVzT6D-yhZg',543],
   ['where_did_life_go','Doctor Nowhere','0DVdDa0qKoc',150],
@@ -31,7 +31,7 @@ export const DEMO_CHANNELS=[
   ['Other Side of the Box | Award-Winning Horror Short Film','Short of the Week','TGZg1YqXv9o',950],
   ['Portrait of God (Short Horror Film)','Dylan Clark','BI9fKfX5V68',450],
   ['POSSIBLY IN MICHIGAN (1983)','ceceliacondit','iLJNSD3H5sg',706],
-  ['THE CHAIR (Award Winning Horror Short Film Directed by Curry Barker)',"that's a bad idea",'mhazCS14Tas',1463],
+  ['THE CHAIR (Award Winning Horror Short Film Directed by Curry Barker)',\"that's a bad idea\",'mhazCS14Tas',1463],
   ['This House Has People in It','Adult Swim','x-pj8OtyO2I',715],
   ['Unedited Footage of a Bear | Infomercials | Adult Swim','Adult Swim','2gMjJNGg9Z8',629],
   ['Too Many Cooks | Adult Swim','Adult Swim','QrGrOK8oZG8',672],
@@ -44,28 +44,28 @@ export const DEMO_CHANNELS=[
   ['Not My Dog | Horror Short Film','CIAK COMPANY','_QlUUb5UMvA',327]
  ]},
  {n:4,name:'DOCUMENTARIES',channelId:'playlist:PLbBSn4qd5pFE',playlistId:'PLbBSn4qd5pFE',color:'#63a7ff',shows:[
-  ["SATAN'S GUIDE TO THE BIBLE",'SATANSGUIDE','z8j3HvmgpYc',5151],
-  ["MyHouse.WAD - Inside Doom's Most Terrifying Mod",'Power Pak','5wAo54DHDY0',6121],
-  ["TaeKwonDo Rockers vs. Cocaine-Dealing Ninjas: The True Story of 'Miami Connection'",'VICE','z1UmZ4WWspo',1395],
-  ["The Incredibly Strange, Sad Story Behind The World's Most Ambitious Demo Tape",'VICE','1pI24MkekW8',1574],
-  ["On The Road with The World's Most Hyperactive Horror Director",'VICE','c62Ot4L7m94',1496],
-  ["The Worst Movie Ever Made? The True Story of 'Birdemic'",'VICE','uZoFNVhEfpE',1589],
+  [\"SATAN'S GUIDE TO THE BIBLE\",'SATANSGUIDE','z8j3HvmgpYc',5151],
+  [\"MyHouse.WAD - Inside Doom's Most Terrifying Mod\",'Power Pak','5wAo54DHDY0',6121],
+  [\"TaeKwonDo Rockers vs. Cocaine-Dealing Ninjas: The True Story of 'Miami Connection'\",'VICE','z1UmZ4WWspo',1395],
+  [\"The Incredibly Strange, Sad Story Behind The World's Most Ambitious Demo Tape\",'VICE','1pI24MkekW8',1574],
+  [\"On The Road with The World's Most Hyperactive Horror Director\",'VICE','c62Ot4L7m94',1496],
+  [\"The Worst Movie Ever Made? The True Story of 'Birdemic'\",'VICE','uZoFNVhEfpE',1589],
   ['The Mayhem of Jason Miller | Dark Side Of The Cage','VICE and 2 more','3Zc5SrQuZXY',2669],
-  ["SoCal Skate History And Eating At Tony Hawk's Place with Lizzie Armanto",'VICE','SRkP2ewEWMU',1037],
+  [\"SoCal Skate History And Eating At Tony Hawk's Place with Lizzie Armanto\",'VICE','SRkP2ewEWMU',1037],
   ['The Series That Changed Skateboarding Forever | Let It Kill You','VICE','ZL5l9iEI0uE',2734],
   ['Rule Britannia: Inside Britain’s Shoplifting Epidemic','VICE','fJOeJ-BqjCg',1266]
  ]},
  {n:5,name:'BOB ROSS',channelId:'UCxcnsr1R5Ge_fbTu5ajt8DQ',color:'#75d887',shows:[
   ['Gladiolus with CRI® Doug Hallgren | Brush Strokes','Bob Ross','BIEppZQRNWk',1884],
   ['Bob Ross - One Hour Special - Peace Offerings of Summer','Bob Ross','NDpKTkdRBu0',3545],
-  ["Ellen's Beach with CRI® Nicholas Hankins | Brush Strokes",'Bob Ross','LRpaBxkYqaU',2909],
-  ["Brush Strokes Table Talk | Episode 11 | Bob Ross' Most Iconic Quotes",'Bob Ross','kyW35uDde84',1325],
+  [\"Ellen's Beach with CRI® Nicholas Hankins | Brush Strokes\",'Bob Ross','LRpaBxkYqaU',2909],
+  [\"Brush Strokes Table Talk | Episode 11 | Bob Ross' Most Iconic Quotes\",'Bob Ross','kyW35uDde84',1325],
   ['Twilight Meadow with CRI® Doug Hallgren | Brush Strokes','Bob Ross','PqJQc4BVgUE',1973],
   ['Happy Little Threes: Summer Bob | Episode 82 | The Joy of Bob Ross: A Happy Little Podcast®','Bob Ross','twH7MR2QJK8',1579],
   ['Daffodil with CRI® Carolyn Saletto | Brush Strokes','Bob Ross','ShEx_7hlCfk',2430],
   ['Bob Ross: Top 10 Summer Paintings','Bob Ross','Uk9BksOB8a4',15430],
   ['A Day at the Beach with CRI® Nicholas Hankins | Brush Strokes','Bob Ross','HbGMWmAR2gk',1750],
-  ["A Few Of Bob's Favorite Things | Episode 81 | The Joy of Bob Ross: A Happy Little Podcast®",'Bob Ross','OmgZ49F9O8k',1292],
+  [\"A Few Of Bob's Favorite Things | Episode 81 | The Joy of Bob Ross: A Happy Little Podcast®\",'Bob Ross','OmgZ49F9O8k',1292],
   ['Brush Strokes Table Talk | Episode 10 | Painting Inspiration','Bob Ross','KGYpu61BGAA',1509],
   ['Peachy Rose with CRI® Doug Hallgren | Brush Strokes','Bob Ross','EL5C1TWKWYU',2358]
  ]}
